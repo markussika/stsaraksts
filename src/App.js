82a657036889f4d66381d42b0db00ea87bc3fc0a
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-10 11:40:34 +0200

    yippy sakam seit kko darit!!!!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 
 import Diena from "./Diena";
+import { useEffect, useState } from "react";
 
 
 
 function App() {
+  const [lessons, setLessons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+async function getLessons() {
+  const response = await fetch("http://cheese-cake.onthewifi.com/api/lessons")
+  const data = await response.json();
+  console.log(data.DP20[4]);
+  const cleansedData = [
+    {
+    diena : "pirmdiena",
+    stundas: data.DP20[0].classes,
+  },
+  {
+    diena : "otrdiena",
+    stundas: data.DP20[1].classes,
+  },
+  {
+    diena : "tresdiena",
+    stundas: data.DP20[2].classes,
+  },
+  {
+    diena : "ceturtdiena",
+    stundas: data.DP20[3].classes,
+  },
+  {
+    diena : "piektdiena",
+    stundas: data.DP20[4].classes,
+  }
+  
+]
+  setLessons(data);
+  setLoading(false);
+
+}
+getLessons();
+
+  
+    
+  })
+ 
   
       const visasStundas = [
         {diena: "pirmdiena", 
@@ -16,10 +57,10 @@ function App() {
         "krivu valdoa",
          "tava mamma",
           "pavarstunda"]},
-        {diena: "tresdinea", 
+        {diena: "tresdiena", 
         stundas:["padzilinatais kurss",
         "klases stunda",
-         "fiika",
+         "fizika",
           "dabaszinibas"]},
         {diena: "ceturtdiena", 
         stundas:["sports",
@@ -27,8 +68,8 @@ function App() {
          "ekskursija",
           "smaidins"]},
         {diena: "piektdiena", 
-        stundas:["patstavigais",
-        "datorsistemas unb datortikli",
+        stundas:["patstavigais darbs",
+        "datorsistemas un datortikli",
          ]}
       ];
       const dienasJSX = visasStundas.map((diena, indekss) =>{
@@ -40,8 +81,9 @@ function App() {
     
     
       <div >
-        {dienasJSX}
+        {loading ? <p>Loading...</p> : dienasJSX}
         </div>
+        
     </>
   )
   
